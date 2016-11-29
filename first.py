a678e0bc423c3b44c1cb5f99bd6eba3d8f814b30
saravibhask <saravibhask@gmail.com>
2016-11-29 11:31:19 -0700

    Updated code
    Updated code including code clean up.

--- first.py
+++ first.py
@@ -5,63 +5,52 @@ tupleExample = ('Savith', 123, 88.88)
 dictExample = {'name':'Savith', 'address':'123 Blue street'}
 listExample = ['Kumar', 456, 99.99]
 
-print (dictExample)
-print (dictExample.keys())
-print (dictExample.values())
-print (dictExample['name'])
+print(dictExample)
+print(dictExample.keys())
+print(dictExample.values())
+print(dictExample['name'])
 
-print (tupleExample)
-print (tupleExample[2])
+print(tupleExample)
+print(tupleExample[2])
 
+print(listExample)
+print(listExample[2])
 
-print (listExample)
-print (listExample[2])
-
-print (len(listExample))
+print(len(listExample))
 
 for x in listExample:
-	print (x)
-
+	print(x)
 
 dictExample = {'Fname':'Savith', 'Lname':'Kumar', 'Address Line 1':'123 Blue St', 'City':'Green City', 'Zip':'12345'}
 
 print(dictExample)
 
 for x in dictExample :
-    print (x, ' : ', dictExample[x])
+    print(x, ' : ', dictExample[x])
 
 listExample = ['abc', 123, 45.67]
 
 for x in listExample :
-    print (x)
+    print(x)
 
 tupleExample = ('xyz', 456, 89.01)
 
+print(time.strftime("%m/%d/%Y"))
 
-print("Hello, World!")
-
-print (time.strftime("%m/%d/%Y"))
-
-print ("Removed the EOF comment")
-
-#added line from sublime text
-
-#added line from codenvy IDE
-
-#
-#strInput = input("enter something :")
-#print(strInput)
-
+print("Removed the EOF comment")
 
 def firstFunction():
-    print("statement within firstFunction()")
-    print("second statement from within firstFunction()")
-print("statement right after firstFunction()")
-
+    print("Statement within firstFunction()")
+    print("Second statement from within firstFunction()")
+print("Statement right after firstFunction()")
 
 firstFunction()
 
-#editing from local - personal laptop
-print("editing from personal laptop - local IDE (sublime)")
+print("Editing from personal laptop - local IDE (sublime)")
 
-print("last instruction in the program.")
+pi = 3.14
+radius = 5
+result = float(4/3*pi*radius**3)
+print('Volume of a sphere of radius ', radius, ', is :', result)
+
+print("Last instruction in the program.")
